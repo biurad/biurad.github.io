0ad2b2b0684e929fc7041eac1d2c784238ced137
divineniiquaye <divineniiquaye@users.noreply.github.com>
2020-09-14 22:25:06 +0000

    Downgraded setup-node to v1 for github pages workflow 95938ac8a854197a8911e3039af1419ba3dc2fdf

---
+++ precache-manifest.d74bd8ddc6cc6874a4e9ec09ae0df949.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0f9d30799bacdbcb08828de0857da6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4228583568b0c5b96ed0",
+    "url": "/static/css/2.1986159d.chunk.css"
+  },
+  {
+    "revision": "6ff81b9fd8f17dc5193d",
+    "url": "/static/css/main.89d2b625.chunk.css"
+  },
+  {
+    "revision": "4228583568b0c5b96ed0",
+    "url": "/static/js/2.a9603526.chunk.js"
+  },
+  {
+    "revision": "a74dc97a784564fdc24a2a4ddd02c9b9",
+    "url": "/static/js/2.a9603526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff81b9fd8f17dc5193d",
+    "url": "/static/js/main.6badc360.chunk.js"
+  },
+  {
+    "revision": "f7411ff5b40731302a70",
+    "url": "/static/js/runtime-main.374ae508.js"
+  },
+  {
+    "revision": "b77931ee8e72c9c982bc8dec9e4d48be",
+    "url": "/static/media/header-img.b77931ee.png"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/static/media/ionicons.99b86349.woff"
+  },
+  {
+    "revision": "a558ac78b554eefa181737749366a91c",
+    "url": "/static/media/ionicons.a558ac78.eot"
+  },
+  {
+    "revision": "d659209138fc7c28c23a48496bdd1c8b",
+    "url": "/static/media/ionicons.d6592091.svg"
+  },
+  {
+    "revision": "ef4a9f280b0e411ddf6c930a0a37c2b0",
+    "url": "/static/media/ionicons.ef4a9f28.ttf"
+  },
+  {
+    "revision": "9454eabfdfd3cdf072e3bd780513f2d7",
+    "url": "/static/media/news1.9454eabf.png"
+  },
+  {
+    "revision": "9454eabfdfd3cdf072e3bd780513f2d7",
+    "url": "/static/media/news2.9454eabf.png"
+  }
+]);
