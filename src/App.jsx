aba6ff5a33e2c1f64863a7dc26476a0c5848caad
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-09-17 07:30:42 +0000

    Modified entry component to use react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,21 @@
-import React from "react";
-import NavSection from "./components/NavSection";
-import HomeSection from "./components/HomeSection";
-import AboutSection from "./components/AboutSection";
-import ProcessSection from "./components/ProcessSection";
-import FooterSection from "./components/FooterSection";
-import PortfolioSection from "./components/PortfolioSection";
-import ServicesSection from "./components/ServicesSection";
-import ContactSection from "./components/ContactSection";
-import NewsSection from "./components/NewsSection";
-import $ from 'jquery';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 import './scripts/bootstrap';
 import './theme/index.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menus: [
-        {"name": "Home", "link": "#home"},
-        {"name": "About", "link": "#about"},
-        {"name": "Portfolio", "link": "#portfolio"},
-        {"name": "Services", "link": "#services"},
-        {"name": "News", "link": "#news"},
-        {"name": "Contact us", "link": "#contact"},
-      ]
-    };
-  }
-
-  componentDidMount() {
-    // smooth scroll
-    $("a").on("click", function (event) {
-
-      if (this.hash !== "") {
-        event.preventDefault();
-
-        let hash = this.hash;
-
-        $("html, body").animate({
-
-          scrollTop: $(hash).offset().top - 50
-
-        }, 850);
-
-      }
-
-    });
-  }
-
+export default class App extends Component {
   render() {
+    // specify base href from env varible 'PUBLIC_URL'
+    // use only if application isn't served from the root
+    // for development it is forced to root only
+    /* global PUBLIC_URL */
+    const basename = process.env.NODE_ENV === 'development' ? '/' : (PUBLIC_URL || '/');
+
     return (
-      <>
-        <NavSection data={this.state.menus} />
-        <HomeSection />
-        <AboutSection />
-        <ProcessSection />
-        <PortfolioSection />
-        <ServicesSection />
-        <NewsSection />
-        <ContactSection />
-        <FooterSection />
-      </>
+      <BrowserRouter basename={basename}>
+        <Routes />
+      </BrowserRouter>
     );
   }
 }
