f6df16fda9c5bad1a3d11a1dae1bc9d8a0b2407c
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-09-17 07:25:02 +0000

    Added NotFound component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../theme/notfound.css';
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div className="box">
+                <div className="box__ghost">
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+
+                    <div className="box__ghost-container">
+                        <div className="box__ghost-eyes">
+                            <div className="box__eye-left"></div>
+                            <div className="box__eye-right"></div>
+                        </div>
+                        <div className="box__ghost-bottom">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className="box__ghost-shadow"></div>
+                </div>
+
+                <div className="box__description">
+                    <div className="box__description-container">
+                        <div className="box__description-title">Whoops!</div>
+                        <div className="box__description-text">It seems like we couldn't find the page you were looking for</div>
+                    </div>
+
+                    <Link to="./" className="box__button">Go back</Link>
+
+                </div>
+
+            </div>
+        )
+    }
+}
