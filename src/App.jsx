e6c6a47728c99fc67fcf5f6b4d04ca78a270f434
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-09-17 08:13:05 +0000

    Fixed ReferenceError: PUBLIC_URL

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Routes from './Routes';
 import './scripts/bootstrap';
 import './theme/index.css';
 
+const PUBLIC_URL = window.PUBLIC_URL = '/';
+
 export default class App extends Component {
   render() {
     // specify base href from env varible 'PUBLIC_URL'
     // use only if application isn't served from the root
     // for development it is forced to root only
     /* global PUBLIC_URL */
-    const basename = process.env.NODE_ENV === 'development' ? '/' : (PUBLIC_URL || '/');
+    const basename = process.env.NODE_ENV === 'developme' ? '/' : (PUBLIC_URL || '/');
 
     return (
       <BrowserRouter basename={basename}>
